73f63488f48c68e608e634dfb9afc585e8c33d01
Jon Edwards <jon@jonedwards.tech>
2021-03-11 14:19:47 +0100

    Trying another json experiment

--- assets/indexJ.js
+++ assets/indexJ.js
@@ -1,41 +1,16 @@
-// get the form element from dom
-const formElement = document.querySelector('form#forms')
-
-// convert the form to JSON
-const getFormJSON = (form) => {
-  const data = new FormData(form);
-  return Array.from(data.keys()).reduce((result, key) => {
-    if (result[key]) {
-      result[key] = data.getAll(key)
-      return result
-    }
-    result[key] = data.get(key);
-    return result;
-  }, {});
-};
-
-// handle the form submission event, prevent default form behaviour, check validity, convert form to JSON
-const handler = (event) => {
+function handleFormSubmit(event) {
   event.preventDefault();
-  const valid = formElement.reportValidity();
-  if (valid) {
-    const result = getFormJSON(formElement);
-    // handle one, multiple or no files uploaded
-    const images = [result.images].flat().filter((file) => !!file.name)
-    // handle one, multiple or no languages selected
-    const languages = [result.languages || []].flat();
-    // convert the checkbox to a boolean
-    const isHappyReader = !!(result.isHappyReader && result.isHappyReader === 'on')
+  
+  const data = new FormData(event.target);
+  
+  const formJSON = Object.fromEntries(data.entries());
 
-    // use spread function, but override the keys we've made changes to
-    const output = {
-      ...result,
-      images,
-      languages,
-      isHappyReader
-    }
-    console.log(output)
-  }
+  // for multi-selects, we need special handling
+  formJSON.snacks = data.getAll('snacks');
+  
+  const results = document.querySelector('.results pre');
+  results.innerText = JSON.stringify(formJSON, null, 2);
 }
 
-formElement.addEventListener("submit", handler)
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', handleFormSubmit);
